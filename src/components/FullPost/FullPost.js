34a37c583792019c620e2057d88fcf687ce3a4d9
David Miller <hawky454@gmail.com>
2019-04-12 12:12:07 -0500

    got my RESTful API setup using JSONPLACEHOLDER

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './FullPost.css';
 
+
 class FullPost extends Component {
+  state = {
+    loadedPost: null
+  }
+
+  componentDidUpdate() {
+    if (this.props.id) {
+      axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        .then(res => {
+          this.setState({
+            loadedPost: res.data
+          })
+          console.log('[FullPost.js] res.data:', res.data);
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+
   render() {
-    let post = <p>Please select a Post!</p>;
-    post = (
-      <div className="FullPost">
-        <h1>Title</h1>
-        <p>Content</p>
-        <div className="Edit">
-          <button className="Delete">Delete</button>
+    let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = <p style={{ textAlign: 'center' }}>Loading...</p>
+    }
+    if (this.state.loadedPost) {
+      post = (
+        <div className="FullPost">
+          <p style={{ color: '#3d3d92' }}>Post Number: {this.props.id}</p>
+          <h1>Title: {this.state.loadedPost.title}</h1>
+          <span style={{ fontWeight: 'bold' }}>Content:</span>
+          <p>{this.state.loadedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+
     return post;
   }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,58 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null
+  }
+
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        // const limitPosts = response.data.slice(0, 6)
+        //! This allows you to control how many posts you receive
+        const updatedPosts = response.data.slice(0, 6).map(post => {
+          return {
+            ...post,
+            author: 'David'
+          }
+        })
+        this.setState({ posts: updatedPosts })
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({
+      selectedPostId: id
+    })
+  }
+
   render() {
+    const posts = this.state.posts.map(post => {
+      return <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        whenClicked={() => this.postSelectedHandler(post.id)}
+      />
+    })
+
+
     return (
       <div>
         <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
+          {posts}
         </section>
         <section>
-          <FullPost />
+          <FullPost
+            id={this.state.selectedPostId}
+            title={this.state.posts}
+          />
         </section>
         <section>
           <NewPost />
